68794836c6845854bdd3fdba775e880b944f23a2
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-11-03 18:33:36 +0530

    login done

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -1,18 +1,55 @@
+"use client";
 import Link from "next/link";
 import styles from "./login.module.css"
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Login = () =>{
+    const [email,changeEmail]=useState("");
+    const [password,changePassword]=useState("");
+    const router = useRouter();
+    const HandleEmailChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        changeEmail(e.target.value);
+    }
+
+    const HandlePasswordChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        changePassword(e.target.value);
+    }
+
+    const onsubmit = async(e:React.ChangeEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        try{    
+            const response = await fetch('/api/auth',{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({
+                    email:email,
+                    password:password
+                })
+            })
+            if(response.ok){
+                const data = await response.json();
+                router.push(`/home/${data.username}`)
+            }
+        }catch(e){
+            console.log(e);
+        }
+    }
+
     return(
+
         <div className={styles.signupGrandParent}>
             <div className={styles.signupParent}>
                     <div className={styles.signupdiv}>Login</div>
                     <div className={styles.subheadsignup}>Start Booking Your Destination!</div>
                     <div>
-                        <form>
+                        <form action="POST" onSubmit={onsubmit}>
 
-                            <div><input placeholder="Username"></input></div>
+                            <div><input placeholder="Email" type="text" value={email} onChange={HandleEmailChange}></input></div>
                             
-                            <div><input placeholder="Password"></input></div>
+                            <div><input placeholder="Password" type="text" value={password} onChange={HandlePasswordChange}></input></div>
 
 
                             <button type="submit">Login!</button>
--- my-app/src/components/navbar/navbar.tsx
+++ my-app/src/components/navbar/navbar.tsx
@@ -1,6 +1,34 @@
+"use client";
 import Link from "next/link";
 import styles from "./navabar.module.css"
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+interface UserData {
+    username: string;
+    name: string;
+    // Add other properties as needed
+}
 const Navbar = () =>{
+    const pathname = usePathname();
+    const username = pathname.split("/").pop();
+    const [user,setUser] = useState<UserData | null>(null);
+    useEffect(()=>{
+        const fetchdata=async()=>{
+            try{
+                const response = await fetch(`/api/getUser/username?username=${username}`)
+                if(!response.ok){
+                    console.log("Error fetching user data");
+                    return;
+                }
+                const userData = await response.json();
+                setUser(userData);
+            }catch(e){
+                console.log("Error while fetching the data",e);
+            }
+            
+        }
+        fetchdata();
+    },[username])
     return(
         <div className={styles.navbarGrandParent}>
             <div className={styles.navbarParent}>
@@ -10,8 +38,8 @@ const Navbar = () =>{
             </div></Link>
 
             <div className={styles.menuset}>
-                <span className={styles.usernamespan}></span>
-                <Link href="/login" style={{textDecoration:"none", color:"#EC5B24",fontWeight:"600"}}><div className={styles.loginBut}>Login/Signup</div></Link>
+                
+                <Link href="/login" style={{textDecoration:"none", color:"#EC5B24",fontWeight:"600"}}><div className={styles.loginBut}>{user?"Hi!":""} {user?user.username:"Login/Signup"}</div></Link>
             </div>
             </div>
         </div>
