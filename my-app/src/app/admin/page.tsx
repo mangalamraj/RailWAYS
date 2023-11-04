e6d695da5a731076b2072e4c828eab28b7572ac8
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2023-11-04 16:35:09 +0530

    Trains schema created

---
+++ my-app/src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './admin.css'
+
+const page = () => {
+  return (
+    <div className="ad_main">
+        <div>add</div>
+    </div>
+  )
+}
+
+export default page
