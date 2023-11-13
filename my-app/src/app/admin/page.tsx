d7856430629cc776b218533a3c28391c3eeac034
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-11-14 03:25:17 +0530

    Train Deletion Done

--- my-app/src/app/admin/page.tsx
+++ my-app/src/app/admin/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react'
 import './admin.css'
 import styles from "./admin.module.css"
 import Link from 'next/link'
-
+import TrainAdminCard from '@/components/trainsAdminCard/TrainAdminCard'
+interface TrainData{
+  
+  name: string;
+  train_no: string;
+  start_destination: string;
+  end_destination: string;
+  start_time: string;
+  end_time: string;
+  seat_1a: string;
+  seat_2a: string;
+  seat_3a: string;
+  seat_sl: string;
+  seat_tatkal_1a: string;
+  seat_tatkal_2a: string;
+  seat_tatkal_3a: string;
+  seat_tatkal_sl: string;
+  
+}
 const page = () => {
+  const [train,setTrain] = useState<TrainData[]>([])
+  useEffect(()=>{
+    const fetchdata=async()=>{
+        try{
+            const response = await fetch(`/api/gettrains`)
+            if(!response.ok){
+                console.log("Error fetching user data");
+                return;
+            }
+            const userData = await response.json();
+            setTrain(userData);
+        }catch(e){
+            console.log("Error while fetching the data",e);
+        }
+
+    }
+    fetchdata();
+},[train])
+
+
   return (
     <div className="ad_main">
       <div className={styles.adminfather}>
@@ -12,6 +51,29 @@ const page = () => {
       <img src='images/plus2.png' width="15px"></img>
       <div className={styles.add}><Link className="main-up" href="/addTrains" style={{textDecoration:"none"}}>Add Trains</Link></div>
       </div>
+
+      </div>
+      <div className={styles.train_set}>
+      {train.map((category, index) => (
+        <div className="tc-sub-user" key={index}>
+          <TrainAdminCard
+            name={category.name}
+            trainNo={category.train_no}
+            stDest={category.start_destination}
+            edDest={category.end_destination}
+            stTime={category.start_time}
+            edTime={category.end_time}
+            s_1a={category.seat_1a}
+            s_2a={category.seat_2a}
+            s_3a={category.seat_3a}
+            s_sl={category.seat_sl}
+            ts_1a={category.seat_tatkal_1a}
+            ts_2a={category.seat_tatkal_2a}
+            ts_3a={category.seat_tatkal_3a}
+            ts_sl={category.seat_tatkal_sl}
+          />
+        </div>
+      ))}
       </div>
 
     </div>
